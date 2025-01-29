43989c9ad86983a9b22a762363335192f60f3909
TheBunnyMan123 <bunny@tkbunny.net>
2025-01-29 14:54:30 -0600

    Pat every frame

--- scripts/common/pat.lua
+++ scripts/common/pat.lua
@@ -1,5 +1,7 @@
 local plrEvents, headEvents, config = require("libs.TheKillerBunny.BunnyPat")
 
 config.unsafeVariables = true
+config.patpatHoldTime = 0
+config.infinipat = true
 config.patRange = 1000
 
